448fd1de3283f665e498e053cf6a96ea0c96c3e9
chandrahas <chandrahas.aroori@gmail.com>
2018-01-12 19:44:54 +0530

    Step 2

---
+++ Week1/q2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(int argc, char const *argv[]) {
+  char* word;
+  char* string;
+
+  scanf("%s\n", word);
+  scanf("%s\n", string);
+  return 0;
+}
