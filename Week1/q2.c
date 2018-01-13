c08dcb876a8f62f80e8fc3222fdb4a1806bcd959
chandrahas <chandrahas.aroori@gmail.com>
2018-01-13 10:48:33 +0530

    q2 is solved

--- Week1/q2.c
+++ Week1/q2.c
@@ -3,10 +3,31 @@
 #include<string.h>
 
 int main(int argc, char const *argv[]) {
-  char* word;
-  char* string;
+  // char* word;
+  // char* string;
+
+  char word[10];
+  char string[100];
+
+  int count = 0;
 
   scanf("%s\n", word);
-  scanf("%s\n", string);
+  // printf("%s\n", word );
+  scanf("%100[^\n]s\n", string);
+  // printf("%s\n",string );
+  // fflush(stdin);
+
+  char* token = strtok(string," ");
+  if(strcmp(token,word) == 0) {
+      count++;
+  }
+  while(token != NULL) {
+    if(strcmp(token,word) == 0) {
+        count++;
+    }
+    token = strtok(NULL," ");
+  }
+
+  printf("%d\n",count);
   return 0;
 }
